bcc35cfef21a0ecf96d45d443fbfb334fdb14788
Mahmoud Swehli <mahmoud@moodio.co.uk>
2019-11-21 22:59:31 +0000

    removed testing console prints

--- lib/src/public_client_application.dart
+++ lib/src/public_client_application.dart
@@ -60,10 +60,6 @@ class PublicClientApplication {
   }
 
   MsalException _convertException(PlatformException e) {
-    print(e.code);
-    print(e.details);
-    print(e.message);
-    print(e.toString());
     switch (e.code) {
       case "CANCELLED":
         return MsalUserCancelledException();
